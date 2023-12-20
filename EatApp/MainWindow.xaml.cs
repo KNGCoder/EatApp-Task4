34798cf222c983c60118c2edfd4993e905d83fa2
Figaro Pomni <kngsklad@gmail.com>
2023-12-20 10:48:50 +0300

    Сериализация

--- EatApp/MainWindow.xaml.cs
+++ EatApp/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using EatApp.Model;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,16 +20,68 @@ namespace EatApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Person> persons = new List<Person>();
         public MainWindow()
         {
             InitializeComponent();
 
-            List<Person> persons = new List<Person>();
-            persons.Add(new Person("Лаура", "Laura"));
-            persons.Add(new Person("Лаура1", "Laura1"));
-            persons.Add(new Person("Лаура1", "Laura1"));
+            
+            //persons.Add(new Person("Лаура", "Laura"));
+            //persons.Add(new Person("Лаура1", "Laura1"));
+            //persons.Add(new Person("Лаура1", "Laura1"));
 
             MyData.ItemsSource = persons;
+        }
+
+        private  void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var weatherForecast = new WeatherForecast
+            //{
+            //    Date = DateTime.Parse("2019-08-01"),
+            //    TemperatureCelsius = 25,
+            //    Summary = "Hot",
+            //    SummaryField = "Hot",
+            //    DatesAvailable = new List<DateTimeOffset>()
+            //        { DateTime.Parse("2019-08-01"), DateTime.Parse("2019-08-02") },
+            //    TemperatureRanges = new Dictionary<string, HighLowTemps>
+            //    {
+            //        ["Cold"] = new HighLowTemps { High = 20, Low = -10 },
+            //        ["Hot"] = new HighLowTemps { High = 60, Low = 20 }
+            //    },
+            //    SummaryWords = new[] { "Cool", "Windy", "Humid" }
+            //};
+
+            //var options = new JsonSerializerOptions { WriteIndented = true };
+            //string jsonString = JsonSerializer.Serialize(weatherForecast, options);
+
+            Person person = new Person
+            {
+                PersonName = "Лаура 01",
+                PersonLogin = "01"
+            };
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(person, options);
+
+            MessageBox.Show(jsonString);
+
+        }
+
+        public class WeatherForecast
+        {
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureCelsius { get; set; }
+            public string? Summary { get; set; }
+            public string? SummaryField;
+            public IList<DateTimeOffset>? DatesAvailable { get; set; }
+            public Dictionary<string, HighLowTemps>? TemperatureRanges { get; set; }
+            public string[]? SummaryWords { get; set; }
+        }
+
+        public class HighLowTemps
+        {
+            public int High { get; set; }
+            public int Low { get; set; }
         }
     }
 }
--- EatApp/Model/Person.cs
+++ EatApp/Model/Person.cs
@@ -6,13 +6,9 @@ using System.Threading.Tasks;
 
 namespace EatApp.Model
 {
+    
    public class Person
     {
-        public Person(string? personName, string? personLogin)
-        {
-            PersonName = personName;
-            PersonLogin = personLogin;
-        }
 
         public string? PersonName { get; set; }
         public string? PersonLogin { get; set; }
